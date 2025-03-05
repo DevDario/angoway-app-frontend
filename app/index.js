4208d0ddf4a120c524d0b053e1feb004ca8e6dda
Dário Silva <dariosilva13222@gmail.com>
2025-03-05 15:21:32 +0100

    feat: settig up routes for stack and tabs navigation

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import { View,Text } from "react-native";
+import {Login} from "./auth/login"
+
+export default function Index(){
+    return(
+        <View>
+            <Login/>
+        </View>
+    )
+}
---
+++ app/tabs/routes.js
@@ -0,0 +1,9 @@
+import { View,Text } from "react-native";
+
+export default function Routes(){
+    return(
+        <View>
+            <Text>Routes Page</Text>
+        </View>
+    )
+}
