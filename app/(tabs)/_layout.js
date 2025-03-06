e1ddd9d9cfb952859b7abdb8c57825d36dbb28e2
Dário Silva <dariosilva13222@gmail.com>
2025-03-06 13:31:44 +0100

    chore: moves files to 'tabs' directory for tabs navigation

---
+++ app/(tabs)/_layout.js
@@ -0,0 +1,51 @@
+import { Tabs } from "expo-router"
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome"
+import { faClock, faCompass,faMap,faUser } from "@fortawesome/free-regular-svg-icons"
+
+
+export default function TabsLayout(){
+    return(
+        <Tabs
+            screenOptions={{
+                headerShown:false,
+                tabBarStyle:{
+                    backgroundColor:"#fcfcfb",
+                    height:60,
+                    paddingTop:12
+                },
+                tabBarShowLabel:false,
+                tabBarActiveTintColor:"#0C6DFF",
+                tabBarInactiveTintColor:"1ab65c",
+            }}
+        >
+            <Tabs.Screen name="routes"
+                options={{
+                    tabBarIcon: ({color}) => (
+                      <FontAwesomeIcon icon={faCompass} color={color}/>  
+                    )
+                }}
+            />
+            <Tabs.Screen name="schedules"
+                options={{
+                    tabBarIcon: ({color}) => (
+                        <FontAwesomeIcon icon={faClock} color={color}/>
+                    )
+                }}
+            />
+            <Tabs.Screen name="index"
+                options={{
+                    tabBarIcon: ({color}) => (
+                        <FontAwesomeIcon icon={faMap} color={color}/>
+                    )
+                }}
+            />
+            <Tabs.Screen name="profile"
+                options={{
+                    tabBarIcon: ({color}) => (
+                        <FontAwesomeIcon icon={faUser} color={color}/>
+                    )
+                }}
+            />
+        </Tabs>
+    )
+}
