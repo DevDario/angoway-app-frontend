995a8bd165676d117164243c9beee8dd122bfa37
Dário Silva <dariosilva13222@gmail.com>
2025-03-06 19:18:48 +0100

    feat: adds the search bar

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,9 +1,48 @@
-import {SafeAreaView, Text} from "react-native"
+import {SafeAreaView, Text, StyleSheet,View} from "react-native"
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome"
+import Input from "../../components/input"
 
 export default function Index(){
     return(
-        <SafeAreaView>
-            <Text>Mapa Page</Text>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.searchContainer}>
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+                <Input 
+                    placeholder={"onde vamos hoje ?"}
+                    styles={styles.searchInput}
+                />
+            </View>
+            <View style={styles.map}>
+
+            </View>
         </SafeAreaView>
     )
 }
+
+export const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        margin:30
+    },
+    searchContainer:{
+        height:60,
+        width:420,
+        borderColor:"#1111",
+        borderWidth:2,
+        flexDirection:"row",
+        alignItems:"center",
+        paddingHorizontal:30,
+        backgroundColor:"#FFF",
+        borderRadius:50,
+    },
+    searchInput:{
+        paddingLeft:15,
+        paddingTop:10,
+        fontSize:15,
+        width:"100%",
+        borderBottomColor:"#FFF",
+        flexDirection:"row",
+
+    }
+})
