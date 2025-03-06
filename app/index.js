78bafbc277838ad2a0ed1febda91a233e748234e
Dário Silva <dariosilva13222@gmail.com>
2025-03-06 13:34:17 +0100

    feat: redirects user if not authenticated

--- app/index.js
+++ app/index.js
@@ -1,9 +1,28 @@
-import {SafeAreaView, Text} from "react-native"
+import Login from "./auth/login"
+import { useEffect } from "react";
+import { useRouter } from "expo-router";
 
 export default function Index(){
+
+    const router = useRouter()
+
+    //check if the user is authenticated (mock)
+
+    useEffect(()=>{
+        const timeout = setTimeout(() => {
+            const isLogged = false
+
+            if(isLogged){
+                router.navigate("/routes")
+            }else{
+                router.navigate("auth/login")
+            }
+        }, 1000)
+
+        clearTimeout(timeout)
+    },[])
+
     return(
-        <SafeAreaView>
-            <Text>Mapa Page</Text>
-        </SafeAreaView>
+        <Login/>
     )
 }
