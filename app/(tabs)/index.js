e0dfcbc34aaad39e26b1803fc9a9e8d8722858be
Dário Silva <dariosilva13222@gmail.com>
2025-03-06 19:29:00 +0100

    raw: imports SearchBar component

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,18 +1,12 @@
 import {SafeAreaView, Text, StyleSheet,View} from "react-native"
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome"
-import Input from "../../components/input"
+import SearchBar from "../../components/SearchBar"
 
 export default function Index(){
     return(
         <SafeAreaView style={styles.container}>
-            <View style={styles.searchContainer}>
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
-                <Input 
-                    placeholder={"onde vamos hoje ?"}
-                    styles={styles.searchInput}
-                />
-            </View>
+            <SearchBar 
+                placeholder={"onde vamos hoje?"}
+            />
             <View style={styles.map}>
 
             </View>
@@ -24,25 +18,5 @@ export const styles = StyleSheet.create({
     container:{
         flex:1,
         margin:30
-    },
-    searchContainer:{
-        height:60,
-        width:420,
-        borderColor:"#1111",
-        borderWidth:2,
-        flexDirection:"row",
-        alignItems:"center",
-        paddingHorizontal:30,
-        backgroundColor:"#FFF",
-        borderRadius:50,
-    },
-    searchInput:{
-        paddingLeft:15,
-        paddingTop:10,
-        fontSize:15,
-        width:"100%",
-        borderBottomColor:"#FFF",
-        flexDirection:"row",
-
     }
 })
