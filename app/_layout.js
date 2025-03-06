7b6cb28089e5eb8fed8aa556725bf5f626269014
Dário Silva <dariosilva13222@gmail.com>
2025-03-06 12:58:14 +0100

    feat: sets tabs navigation

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,7 @@
-import { Tabs } from "expo-router";
-import { Icon } from "react-native-paper";
+import { Tabs } from "expo-router"
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome"
+import { faClock, faCompass,faMap,faUser } from "@fortawesome/free-regular-svg-icons"
+
 
 export default function RootLayout(){
     return(
@@ -7,22 +9,43 @@ export default function RootLayout(){
             screenOptions={{
                 headerShown:false,
                 tabBarStyle:{
-                    backgroundColor:"#fcfcfb"
+                    backgroundColor:"#fcfcfb",
+                    height:60,
+                    paddingTop:12
                 },
+                tabBarShowLabel:false,
                 tabBarActiveTintColor:"#0C6DFF",
-                tabBarInactiveTintColor:"1ab65c"
+                tabBarInactiveTintColor:"1ab65c",
             }}
         >
             <Tabs.Screen name="routes"
                 options={{
                     tabBarIcon: ({color}) => (
-                        <Icon key={"Home"}/>
+                      <FontAwesomeIcon icon={faCompass} color={color}/>  
                     )
                 }}
             />
-            <Tabs.Screen name="schedules"/>
-            <Tabs.Screen name="index"/>
-            <Tabs.Screen name="profile"/>
+            <Tabs.Screen name="schedules"
+                options={{
+                    tabBarIcon: ({color}) => (
+                        <FontAwesomeIcon icon={faClock} color={color}/>
+                    )
+                }}
+            />
+            <Tabs.Screen name="index"
+                options={{
+                    tabBarIcon: ({color}) => (
+                        <FontAwesomeIcon icon={faMap} color={color}/>
+                    )
+                }}
+            />
+            <Tabs.Screen name="profile"
+                options={{
+                    tabBarIcon: ({color}) => (
+                        <FontAwesomeIcon icon={faUser} color={color}/>
+                    )
+                }}
+            />
         </Tabs>
     )
 }
