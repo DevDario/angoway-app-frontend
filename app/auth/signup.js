3654385636fc2648fcd0c08e46e93bea06b189b5
Dário Silva <dariosilva13222@gmail.com>
2025-03-06 18:35:14 +0100

    feat: adds a signup form

--- app/auth/signup.js
+++ app/auth/signup.js
@@ -1,9 +1,119 @@
-import { SafeAreaView,Text } from "react-native";
+import { SafeAreaView,Text, StyleSheet,View, Alert, TouchableOpacity } from "react-native";
+import Input from "../../components/input";
+import Button from "../../components/Button";
+import { Link } from "expo-router"
+import { faGoogle, faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 export default function Signup(){
     return(
-        <SafeAreaView>
-            <Text>Signup</Text>
-        </SafeAreaView>
+        <SafeAreaView style={styles.container}>
+                    <View style={styles.header}>
+                        <Text style={styles.headerTitle}>Criar Conta</Text>
+                        <Text style={styles.headerDescription}>Já tem uma conta ? <Link href={"/auth/login"} style={styles.highlight}>Inicie Sessão</Link> </Text>
+                    </View>
+        
+        
+                    <View style={styles.inputContainer}>
+                        <View style={styles.inputLabelContainer}>
+                            <Text style={styles.inputLabel}>Seu Email</Text>
+                            <Input placeholder={""}/>
+                        </View>
+
+                        <View style={styles.inputLabelContainer}>
+                            <Text style={styles.inputLabel}>Seu Número</Text>
+                            <Input placeholder={"+244"}/>
+                        </View>
+        
+                        <View style={styles.inputLabelContainer}>
+                            <Text style={styles.inputLabel}>Senha</Text>
+                            <Input placeholder={""}
+                            />
+                        </View>
+        
+                        <View style={styles.buttonContainer}>
+                                <Link href={"/routes"}>
+                                    <Button text={"Entrar"}
+                                            style={styles.loginButton}/>
+                                </Link>
+                        </View>
+        
+                    </View>
+        
+        
+                    <View style={styles.footerButtons}>
+                        <Text>Ou</Text>
+        
+                        <Link href={"/routes"}>
+                        <Button text={"Entrar com Facebook"}
+                                    icon={faFacebook}
+                                    style={styles.optionLoginButton}
+                                />
+                        </Link>
+        
+                        <Link href={"/routes"}>
+                        <Button text={"Entrar com Google"}
+                                    icon={faGoogle}
+                                    style={styles.optionLoginButton}
+                                />
+                        </Link>
+                    </View>
+        
+                </SafeAreaView>
     )
 }
+
+export const styles = StyleSheet.create({
+    header:{
+        paddingTop:20,
+        paddingBottom:50,
+        paddingLeft:40
+    },
+    headerTitle:{
+        fontSize:25,
+        fontWeight:"700"
+    },
+    headerDescription:{
+        fontSize:16,
+        fontWeight:"200",
+        paddingTop:15
+    },
+    container:{
+        flex:1,
+        paddingTop:20,
+    },
+    inputContainer:{
+        width:"100%",
+        gap:50,
+        paddingLeft:40
+    },
+    inputLabel:{
+        fontSize:15,
+        color:"#ACACAC"
+    },
+    inputLabelContainer:{
+        gap:18,
+        justifyContent:"flex-start",
+        alignItems:"flex-start"
+    },
+    buttonContainer:{
+        paddingTop:10,
+        alignItems:"flex-end",
+        paddingRight:90
+    },
+    loginButton:{
+        width:173
+    },
+    highlight:{
+        color:"#0C6DFF",
+        fontWeight:"700"
+    },
+    footerButtons:{
+        width:"100%",
+        paddingTop:40,
+        gap:20,
+        alignItems:"center"
+    },
+    optionLoginButton:{
+        width:300
+    }
+})
