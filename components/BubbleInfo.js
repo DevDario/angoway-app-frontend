beab7c378c227ce4328378709e09e7fda55c381d
Dário Silva <dariosilva13222@gmail.com>
2025-03-07 12:29:25 +0100

    feat: displays schedule status

---
+++ components/BubbleInfo.js
@@ -0,0 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { StyleSheet, Text, View } from "react-native";
+
+const statusColors = [
+    {status: "ativo",backgroundColor:"#CCFFE4",color:"#12AA59"},
+    {status: "atrasado",backgroundColor:"#FFCFAA",color:"#C26A26"},
+    {status: "cancelado",backgroundColor:"#FFAAAA",color:"#974343"}
+]
+
+export default function BubbleInfo({text,icon,status}){
+
+    const statusDetails = statusColors.find(item => item.status === status) || { status:"default", backgroundColor: "gray", color: "#212121" }
+
+    return(
+        <View style={[styles.container,{backgroundColor: statusDetails.backgroundColor}]}>
+           {icon && <FontAwesomeIcon icon={icon} style={{color:statusDetails.color}}/> }
+            <Text style={[styles.text,{color: statusDetails.color}]}>{text}</Text>
+        </View>
+    )
+}
+
+export const styles = StyleSheet.create({
+    container:{
+        alignItems:"center",
+        alignContent:"center",
+        justifyContent:"center",
+        gap:9,
+        borderRadius:30,
+        width:70,
+        height:30
+    },
+    text:{
+        fontWeight:500
+    }
+})
