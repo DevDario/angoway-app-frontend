05ed9b30ecee5bd7578f6ae7c1032b8866fa69e0
Dário Silva <dariosilva13222@gmail.com>
2025-03-08 10:11:18 +0100

    feat: handles location search

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -3,12 +3,20 @@ import { StyleSheet, View } from "react-native";
 import SearchBar from "../../components/SearchBar";
 
 export default function Index() {
+  const [searchBarValue, setSearchBarValue] = useState("");
+
+  function handleLocationSearch(query){
+    setSearchBarValue(query)
+  }
+
   return (
     <View style={styles.container}>
     <View style={styles.header}>
       <SearchBar 
         placeholder={"onde vamos hoje?"} 
         style={styles.searchBar}
+        onChangeText={handleLocationSearch}
+        value={searchBarValue}
       />
     </View>
     <View style={styles.content}>
