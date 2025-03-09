b596cc291df8f8346ccdff806bd831ba8034042d
Dário Silva <dariosilva13222@gmail.com>
2025-03-09 11:40:20 +0100

    feat: adds returnButton component and a title to the header

--- app/(tabs)/profile.js
+++ app/(tabs)/profile.js
@@ -1,10 +1,37 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import ReturnButton from "../../components/ReturnButton";
+import { useRouter } from "expo-router";
 
 export default function ProfilePage() {
+
+  const router = useRouter()
+
   return (
-    <SafeAreaView>
-      <Text>Profile Page</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View style={styles.pageHeader}>
+        <ReturnButton onPress={() => router.back()}/>
+        <Text style={styles.pageHeaderText}>Seu Perfil</Text>
+      </View>
+    </View>
   );
 }
+
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 30,
+    marginTop: 30
+  },
+  pageHeader: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  pageHeaderText: {
+    fontSize: 16,
+    fontWeight: "700",
+    paddingLeft:85
+  }
+})
