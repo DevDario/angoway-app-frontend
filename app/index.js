7dfed7c1888d6f7b2cc70e5eb05f67c8a4627ddb
Dário Silva <dariosilva13222@gmail.com>
2025-03-09 17:38:49 +0100

    refactor: removes logs and redundant comments

--- app/index.js
+++ app/index.js
@@ -13,16 +13,12 @@ export default function Index() {
   const router = useRouter();
   const [location, setLocation] = useState(null);
 
-  // gets user permission to access location
   async function getLocationPermission() {
     const { granted } = await requestForegroundPermissionsAsync();
 
     if (granted) {
-
       const location = await getCurrentPositionAsync();
       setLocation(location);
-      console.log(location);
-
     } else {
       Alert.alert("Precisamos de permissão para acessar sua localização");
     }
@@ -32,23 +28,18 @@ export default function Index() {
     getLocationPermission();
   }, []);
 
-  // watches user location
-
   useEffect(() => {
     watchPositionAsync({
       accuracy: LocationAccuracy.Highest,
-      timeInterval:1000,
-      distanceInterval:1,
-    },(response)=>{
-      console.log("NEW LOCATION -> ", response)
+      timeInterval: 1000,
+      distanceInterval: 1,
+    }, (response) => {
       setLocation(response)
     })
-  },[])
-  
-  // saves user last known and current location using Async Storage
+  }, [])
+
 
   //check if the user is authenticated (mock)
-
   useEffect(() => {
     const timeout = setTimeout(() => {
       const isLogged = false;
