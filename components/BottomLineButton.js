199c74541da11949b47cedf6df53e582c0745850
Dário Silva <dariosilva13222@gmail.com>
2025-03-09 15:44:08 +0100

    feat: creates button with a differet style

---
+++ components/BottomLineButton.js
@@ -0,0 +1,35 @@
+import { StyleSheet, TouchableOpacity,Text } from "react-native";
+import React from "react";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons"
+
+export default function BottomLineButton({ onPress, style, text }) {
+    return (
+        <TouchableOpacity style={[styles.container,style]} onPress={onPress}>
+            <Text>{text}</Text>
+            <FontAwesomeIcon icon={faChevronRight} size={15} color="#212121"/>
+        </TouchableOpacity>
+    );
+}
+
+BottomLineButton.propTypes = {
+    text: PropTypes.string.isRequired,
+    onPress: PropTypes.func.isRequired,
+    styles: PropTypes.object,
+};
+
+export const styles = StyleSheet.create({
+    container: {
+        borderBottomWidth: 2,
+        borderBottomColor: "#0C6DFF",
+        width: "100%",
+        fontSize: 16,
+        paddingBottom: 13,
+        color: "#212121",
+        textAlign: "left",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignContent:"center"
+    },
+});
