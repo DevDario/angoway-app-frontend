c8dea810098d9b3b1629d84c25b4077ba025be90
Dário Silva <dariosilva13222@gmail.com>
2025-03-09 07:59:08 +0100

    feat: requests user permission to access his location
    feat: Subscribe to location updates from the device

--- app/index.js
+++ app/index.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useRef } from "react";
 import Login from "./auth/login";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "expo-router";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  watchPositionAsync,
+  LocationAccuracy
+} from "expo-location";
 
 export default function Index() {
   const router = useRouter();
+  const [location, setLocation] = useState(null);
+
+  // gets user permission to access location
+  async function getLocationPermission() {
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if (granted) {
+
+      const location = await getCurrentPositionAsync();
+      setLocation(location);
+      console.log(location);
+
+    } else {
+      Alert.alert("Precisamos de permissão para acessar sua localização");
+    }
+  }
+
+  useEffect(() => {
+    getLocationPermission();
+  }, []);
+
+  // watches user location
+
+  useEffect(() => {
+    watchPositionAsync({
+      accuracy: LocationAccuracy.Highest,
+      timeInterval:1000,
+      distanceInterval:1,
+    },(response)=>{
+      console.log("NEW LOCATION -> ", response)
+      setLocation(response)
+    })
+  },[])
+  
+  // saves user last known and current location using Async Storage
 
   //check if the user is authenticated (mock)
 
