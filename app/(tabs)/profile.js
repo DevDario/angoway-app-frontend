760e0227789f41c77e8a1f890a8a899b740aa7c1
Dário Silva <dariosilva13222@gmail.com>
2025-03-09 13:06:10 +0100

    feat: creates section to update user details

--- app/(tabs)/profile.js
+++ app/(tabs)/profile.js
@@ -1,17 +1,69 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Alert } from "react-native";
 import ReturnButton from "../../components/ReturnButton";
 import { useRouter } from "expo-router";
+import ProfileAvatar from "../../components/ProfileAvatar";
+import Input from "../../components/input";
+import Button from "../../components/Button"
 
 export default function ProfilePage() {
-
+  const [phoneNumber, setPhoneNumber] = useState("")
+  const [email, setEmail] = useState("")
   const router = useRouter()
+
+
+  function handleEmail(email) {
+    setEmail(email)
+  }
+
+  function handlePhoneNumber(phoneNumber) {
+    setPhoneNumber(phoneNumber)
+  }
+
+  function handleCredentialsUpdate() {
+    Alert.alert("Alterações Confirmadas","As suas alterações foram guardadas !")
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.pageHeader}>
-        <ReturnButton onPress={() => router.back()}/>
+        <ReturnButton onPress={() => router.back()} />
         <Text style={styles.pageHeaderText}>Seu Perfil</Text>
+      </View>
+
+      <View style={styles.detailsHeader}>
+        <Text style={styles.username}>John Doe</Text>
+        <ProfileAvatar avatarUrl="../../assets/favicon-100x100.png" />
+      </View>
+
+      <View style={styles.credentialsContent}>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>
+            Telefone
+          </Text>
+          <Input
+            value={phoneNumber}
+            placeholder={"+244 933 333 333"}
+            onChangeText={handlePhoneNumber}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>
+            Email
+          </Text>
+          <Input
+            value={email}
+            placeholder={"johndoe@gmail.com"}
+            onChangeText={handleEmail}
+          />
+        </View>
+
+        <Button 
+          text="Salvar Alterações"
+          onPress={handleCredentialsUpdate}
+          style={styles.button}
+        />
       </View>
     </View>
   );
@@ -22,7 +74,8 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 30,
-    marginTop: 30
+    marginTop: 30,
+    alignItems: "center",
   },
   pageHeader: {
     width: "100%",
@@ -32,6 +85,35 @@ export const styles = StyleSheet.create({
   pageHeaderText: {
     fontSize: 16,
     fontWeight: "700",
-    paddingLeft:85
+    paddingLeft: 85
+  },
+  detailsHeader: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 10
+  },
+  username: {
+    fontSize: 22,
+    fontWeight: "700",
+  },
+  credentialsContent: {
+    width: "100%",
+    alignContent:"flex-start",
+    gap:20,
+  },
+  inputContainer: {
+    flexDirection: "column",
+    gap: 10,
+  },
+  inputLabel: {
+    color: "#212121",
+    fontWeight:700
+  },
+  button: {
+    width:"100%",
+    alignItems:"center",
+    backgroundColor: "#0C6DFF",
   }
 })
