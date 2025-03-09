f593509630c5b8c04822b9dfbdadcc95746840bc
Dário Silva <dariosilva13222@gmail.com>
2025-03-09 10:56:18 +0100

    fix: wrapps layout with a View instead of a ScrollView due to the FlatList

--- app/(tabs)/routes.js
+++ app/(tabs)/routes.js
@@ -2,7 +2,6 @@ import React from "react";
 import {
   View,
   Text,
-  ScrollView,
   StyleSheet,
   Alert,
   FlatList,
@@ -11,9 +10,11 @@ import SearchBar from "../../components/SearchBar";
 import { useState } from "react";
 import RouteSuggestionChip from "../../components/RouteSuggestionChip";
 import Button from "../../components/Button";
+import { useRouter } from "expo-router";
 
 export default function RoutesPage() {
   const [searchBarValue, setSearchBarValue] = useState("");
+  const router = useRouter()
 
   function handleRouteSearch() {
     if (searchBarValue.length < 1) {
@@ -23,7 +24,7 @@ export default function RoutesPage() {
       );
       return;
     } else {
-      Alert.alert("🔎 Pesquisando Por", searchBarValue);
+      router.push("/")
     }
   }
 
@@ -32,65 +33,55 @@ export default function RoutesPage() {
   }
 
   const suggestions = [
-    {
-      id: 1,
-      route: "Igreja Pentecostal de Luanda",
-      lat: "-12345",
-      long: "54321",
-    },
+    { id: 1, route: "Igreja Pentecostal de Luanda", lat: "-12345", long: "54321", },
     { id: 2, route: "Zap Cinemas, Kikagil", lat: "-12345", long: "54321" },
-    { id: 3, route: "Arena Fúria, Patriota", lat: "-12345", long: "54321" },
-    { id: 4, route: "Largo das Escolas", lat: "-12345", long: "543212" },
-    { id: 5, route: "Praça da Independência", lat: "-12345", long: "54321" },
+    { id: 3, route: "Arena Fúria, Patriota", lat: "-12345", long: "54321" }
   ];
 
   return (
-    <ScrollView
-      style={[styles.container]}
-      showsHorizontalScrollIndicator={false}
-      showsVerticalScrollIndicator={false}
-      keyboardShouldPersistTaps="handled"
-    >
+    <View style={[styles.container]}>
       <View style={styles.header}>
-        <Text style={styles.logo}>Angoway® </Text>
+        <Text style={styles.logo}>Angoway®</Text>
       </View>
 
-      <View style={styles.content}>
+      <View style={styles.searchBarContent}>
         <SearchBar
           placeholder={"Zap Cinemas, Rua do Kikagil"}
           style={styles.searchBar}
           value={searchBarValue}
           onChangeText={setSearchBarValue}
         />
+      </View>
 
-        <View style={styles.mainContent}>
-          <View style={styles.suggestionsContainer}>
-            <Text style={styles.suggestionsLabel}>Sugestões</Text>
-            <View style={styles.suggestions}>
-              <FlatList
-                data={suggestions}
-                renderItem={({ item }) => (
-                  <RouteSuggestionChip
-                    suggestion={item.route}
-                    key={item.id}
-                    onPress={() => handleSuggestionSelect(item.route)}
-                    style={styles.suggestionChip}
-                  />
-                )}
-                keyExtractor={(item) => item.id}
-                bounces={true}
-              />
-            </View>
+      <View style={styles.content}>
+        <View style={styles.suggestionsContainer}>
+          <Text style={styles.suggestionsLabel}>Sugestões</Text>
+          <View style={styles.suggestions}>
+            <FlatList
+              data={suggestions}
+              renderItem={({ item }) => (
+                <RouteSuggestionChip
+                  suggestion={item.route}
+                  key={item.id}
+                  onPress={() => handleSuggestionSelect(item.route)}
+                  style={styles.suggestionChip}
+                />
+              )}
+              keyExtractor={(item) => item.id}
+              bounces={true}
+            />
           </View>
-
-          <Button
-            text="Procurar"
-            onPress={handleRouteSearch}
-            style={styles.searchButton}
-          />
         </View>
       </View>
-    </ScrollView>
+
+      <Button
+        text="Procurar"
+        onPress={handleRouteSearch}
+        style={styles.searchButton}
+        textColor={"#FFF"}
+      />
+
+    </View>
   );
 }
 
@@ -113,6 +104,10 @@ export const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 700,
   },
+  searchBarContent: {
+    width: "100%",
+    alignItems: "center",
+  },
   content: {
     width: "100%",
     alignItems: "center",
@@ -132,8 +127,7 @@ export const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     gap: 10,
-    flexWrap: "wrap",
-    paddingTop: 15,
+    paddingTop: 10,
   },
   suggestionChip: {
     marginTop: 10,
@@ -141,17 +135,19 @@ export const styles = StyleSheet.create({
   suggestionsLabel: {
     color: "#3333",
     fontSize: 15,
-    fontWeight: 500,
+    fontWeight: 300,
   },
-  mainContent: {
+  content: {
     width: "100%",
-    alignItems: "center",
-    flexDirection: "column",
-    gap: 35,
+    flexDirection: "column"
   },
   searchButton: {
     width: "100%",
     backgroundColor: "#0C6DFF",
-    color: "#fff",
+    fontWeight: 800,
+    marginTop: 30
+  },
+  text: {
+    color: "#FFF"
   },
 });
