657cdd77882d61763c5598c8bf2cdab1f0eedc73
Dário Silva <dariosilva13222@gmail.com>
2025-03-12 02:06:03 +0100

    feat: shows the header and map

--- app/(tabs)/trackingBus.js
+++ app/(tabs)/trackingBus.js
@@ -1,20 +1,59 @@
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import ReturnButton from '../../components/ReturnButton';
+import MapView from 'react-native-maps';
 
 export default function TrackingBus() {
     return (
         <SafeAreaProvider>
-            <SafeAreaView style={styles.centeredView}>
-                <Text>Tracking Bus</Text>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.pageHeader}>
+                    <ReturnButton onPress={() => router.back()} />
+                    <Text style={styles.pageHeaderText}>Rota do Autocarro</Text>
+                </View>
+
+                <MapView
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: -8.8956,
+                        longitude: 13.2272,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                    showsMyLocationButton={true}
+                    showsUserLocation={true}
+                    showsTraffic={true}
+                    zoomEnabled={true}
+                    zoomControlEnabled={true}
+                />
+
             </SafeAreaView>
         </SafeAreaProvider>
     );
 };
 
 export const styles = StyleSheet.create({
-    centeredView: {
+    container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: "center",
+    },
+    pageHeader: {
+        width: "100%",
+        height: "12%",
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: "#fcfcfb",
+        paddingHorizontal: 30,
+        borderBottomWidth: 1,
+        borderBottomColor: "#2222"
+    },
+    pageHeaderText: {
+        fontSize: 16,
+        fontWeight: "700",
+        paddingLeft: 60
+    },
+    map: {
+        width: "100%",
+        height: "60%",
     },
 });
