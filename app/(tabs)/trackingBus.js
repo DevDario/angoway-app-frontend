fd71fe0ca082d767f5b339a0239efe68ede5e310
Dário Silva <dariosilva13222@gmail.com>
2025-03-13 08:36:49 +0100

    feat: renders  dynamic messaged above the RouteCard component

--- app/(tabs)/trackingBus.js
+++ app/(tabs)/trackingBus.js
@@ -1,10 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import ReturnButton from '../../components/ReturnButton';
 import MapView from 'react-native-maps';
 import { router } from 'expo-router';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import RouteCard from '../../components/RouteCard';
+import { useState } from 'react';
 
 export default function TrackingBus() {
+    const [showBusInfo, setShowBusInfo] = useState(false)
+
+    const route = require("../../mockdata.json").trackingRoutes[0]
+
     return (
         <SafeAreaProvider>
             <SafeAreaView style={styles.container}>
@@ -24,9 +32,40 @@ export default function TrackingBus() {
                     showsMyLocationButton={true}
                     showsUserLocation={true}
                     showsTraffic={true}
-                    zoomEnabled={true}
-                    zoomControlEnabled={true}
+                    showsCompass={false}
                 />
+
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                    showsHorizontalScrollIndicator={false}
+                    keyboardShouldPersistTaps={"handled"}
+                    style={styles.scrollView}
+                >
+                    <View style={styles.trackingInfoContent}>
+                        <RouteCard
+                            key={route.id}
+                            routeDetails={route}
+                        />
+
+                        <View style={styles.busInfoContainer}>
+                            {showBusInfo ? (
+                                <>
+                                    <FontAwesomeIcon icon={faInfoCircle} size={15} color='#4444' /><Text style={styles.hintText}>
+                                        Clique num autocarro para ver os detalhes
+                                    </Text>
+                                </>
+                            ) : (
+                                <>
+                                    <FontAwesomeIcon icon={faInfoCircle} size={15} color='#4444' /><Text style={styles.hintText}>
+                                        info do autocarro
+                                    </Text>
+                                </>
+                            )}
+                        </View>
+
+                    </View>
+
+                </ScrollView>
 
             </SafeAreaView>
         </SafeAreaProvider>
@@ -57,4 +96,26 @@ export const styles = StyleSheet.create({
         width: "100%",
         height: "60%",
     },
-});
+    scrollView: {
+        position: "absolute",//FIX: results in loosing the hability to scroll the view
+        top: "63%",
+        height: "100%"
+    },
+    trackingInfoContent: {
+        width: "100%",
+        height: "100%",
+        alignContent: "flex-start",
+        gap: 20,
+    },
+    busInfoContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 5,
+        paddingBottom: 20
+    },
+    hintText: {
+        color: "#4444",
+        fontWeight: 300
+    }
+})
