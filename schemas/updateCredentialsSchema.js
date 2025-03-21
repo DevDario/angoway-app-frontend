27366b23fed0c997cd11fd53b013c9a510573ec7
Dário Silva <dariosilva13222@gmail.com>
2025-03-21 15:18:46 +0100

    feat: validates form when updating credentials

---
+++ schemas/updateCredentialsSchema.js
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const updateCredentialsSchema = z.object({
+    email: z
+        .string()
+        .nonempty("Insira o novo e-mail para atualizar")
+        .email("E-mail inválido"),
+    phoneNumber: z
+        .string()
+        .nonempty("Insira o novo número para atualizar")
+        .min(9, "O número deve ter pelo menos 9 dígitos")
+        .max(9, "O número deve ter no máximo 9 dígitos")
+        .regex(/^(?:\+244|244)?9[1-9]\d{7}$/, "Formato de telefone inválido (deve ser 9xx xxx xxx)"),
+})
