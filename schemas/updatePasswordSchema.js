adc867120ac77c75cc19085516dbcb229da38a3b
Dário Silva <dariosilva13222@gmail.com>
2025-03-21 15:32:37 +0100

    feat: validates form when updating password

---
+++ schemas/updatePasswordSchema.js
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const updatePasswordSchema = z.object({
+    password: z
+        .string()
+        .min(8, "A senha deve ter pelo menos 8 caracteres")
+        .regex(/[A-Z]/, "A senha deve conter pelo menos uma letra maiúscula")
+        .regex(/[a-z]/, "A senha deve conter pelo menos uma letra minúscula")
+        .regex(/\d/, "A senha deve conter pelo menos um número"),
+    confirmPassword: z
+        .string(),
+}).refine(data => data.password === data.confirmPassword, {
+    message: "As senhas não coincidem",
+    path: ["confirmPassword"],
+});
