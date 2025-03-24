989be1a52aac9aad74932a4edef959cee19d75be
Dário Silva <dariosilva13222@gmail.com>
2025-03-24 21:55:00 +0100

    chore: allows credentials on requests

--- app/api/axiosInstance.js
+++ app/api/axiosInstance.js
@@ -2,7 +2,8 @@ import axios from "axios"
 
 export const api = axios.create({
     baseURL: process.env.EXPO_PUBLIC_API_BASE_URL || "http://localhost:3000",
+    withCredentials: true,
     headers: {
         "Content-Type": "application/json"
-    }
+    },
 })
