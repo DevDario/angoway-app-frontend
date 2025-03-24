57b1a5514d44c5f4c0e2870025ef9298de78ac48
Dário Silva <dariosilva13222@gmail.com>
2025-03-24 21:58:14 +0100

    feat(major): saves token using MMKV and redirects users to pages based on status
    chore: exports all functions for external use
    
    chore: invalidate querys after request is done
    
    chore: check if the access token is present

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,14 +1,69 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { loginUseCase, signupUseCase } from "../app/api/auth";
+import { useEffect, useState } from "react";
+import { getToken, saveToken, removeToken } from "../utils/secureStore";
+import { useRouter } from "expo-router";
 
-export const useLogin = () => {
-    return useMutation({
-        mutationFn: loginUseCase
-    })
-}
+export function useAuth() {
+    const queryClient = useQueryClient()
+    const router = useRouter()
+    const [authToken, setAuthToken] = useState(null)
+    const [isCheckingAuth, setIsCheckingAuth] = useState(true);
 
-export const useSignup = () => {
-    return useMutation({
-        mutationFn: signupUseCase
+
+    useEffect(() => {
+        async function checkToken() {
+            const token = getToken();
+            setAuthToken(token);
+            setIsCheckingAuth(false)
+        }
+        checkToken()
+    }, []);
+
+    const useLogin = useMutation({
+        mutationFn: loginUseCase,
+        onMutate: () => {
+            setIsCheckingAuth(true)
+        },
+        onSuccess: async (data) => {
+            saveToken(data.access_token)
+            setAuthToken(data.access_token)
+            router.replace("/routes")
+            queryClient.invalidateQueries(["user"])
+        },
+        onSettled: () => {
+            setIsCheckingAuth(false)
+        },
     })
+
+    const useSignup = useMutation({
+        mutationFn: signupUseCase,
+        onMutate: () => {
+            setIsCheckingAuth(true)
+        },
+        onSuccess: async (data) => {
+            saveToken(data.access_token)
+            setAuthToken(data.access_token)
+            router.replace("/routes")
+            queryClient.invalidateQueries(["user"])
+        },
+        onSettled: () => {
+            setIsCheckingAuth(false)
+        },
+    })
+
+    const logout = async () => {
+        removeToken();
+        setAuthToken(null);
+        router.replace("/auth/login")
+        queryClient.clear();
+    }
+
+    return {
+        useLogin,
+        useSignup,
+        logout,
+        authToken,
+        isCheckingAuth
+    }
 }
