932c6d92020a6f38618351d7cc5a8a05f584cf68
Dário Silva <dariosilva13222@gmail.com>
2025-03-24 19:55:42 +0100

    refactor: changes API base URL value

--- app/api/axiosInstance.js
+++ app/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: process.env.API_BASE_URL,
+    baseURL: process.env.EXPO_PUBLIC_API_BASE_URL || "http://localhost:3000",
     headers: {
         "Content-Type": "application/json"
     }
