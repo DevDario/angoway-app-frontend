8ea7525e8f5189da3529f6f661e78a8554e9e537
Dário Silva <dariosilva13222@gmail.com>
2025-03-25 00:44:22 +0100

    feat: adds name and disability fields validations

--- schemas/signupSchema.js
+++ schemas/signupSchema.js
@@ -1,10 +1,14 @@
 import { z } from "zod";
 
 export const signupSchema = z.object({
+    name: z
+        .string()
+        .min(3, "Seu nome deve ter pelo menos 3 caracteres")
+    ,
     email: z
         .string()
         .email("E-mail inválido"),
-    phoneNumber: z
+    number: z
         .string()
         .min(9, "O número deve ter pelo menos 9 dígitos")
         .max(9, "O número deve ter no máximo 9 dígitos")
@@ -14,5 +18,8 @@ export const signupSchema = z.object({
         .min(8, "A senha deve ter pelo menos 8 caracteres")
         .regex(/[A-Z]/, "A senha deve conter pelo menos uma letra maiúscula")
         .regex(/[a-z]/, "A senha deve conter pelo menos uma letra minúscula")
-        .regex(/\d/, "A senha deve conter pelo menos um número")
+        .regex(/\d/, "A senha deve conter pelo menos um número"),
+    disability: z
+        .string()
+        .optional(true)
 })
