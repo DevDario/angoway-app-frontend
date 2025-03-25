6cccc616b4abb909f0a3cef0471730a860e741ba
Dário Silva <dariosilva13222@gmail.com>
2025-03-25 15:40:51 +0100

    feat: creates custom hook for profile-only related requests

---
+++ hooks/useProfile.js
@@ -0,0 +1,49 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateCredentialsUseCase } from "../app/api/profile";
+import { useEffect, useState } from "react";
+import { getToken } from "../utils/secureStore";
+import { useRouter } from "expo-router";
+
+export function useProfile() {
+    const queryClient = useQueryClient()
+    const router = useRouter()
+    const [authToken, setAuthToken] = useState(null)
+    const [isCheckingAuth, setIsCheckingAuth] = useState(true);
+    const [authError, setAuthError] = useState(null);
+
+
+    useEffect(() => {
+        async function checkToken() {
+            const token = getToken();
+            setAuthToken(token);
+            setIsCheckingAuth(false)
+        }
+        checkToken()
+    }, []);
+
+    const useUpdateCredentials = useMutation({
+        mutationFn: updateCredentialsUseCase,
+        onMutate: () => {
+            setIsCheckingAuth(true)
+            setAuthError(null)
+        },
+        onSuccess: async () => {
+            setIsCheckingAuth(true)
+            setAuthError(null)
+            router.reload()
+        },
+        onError: async (req) => {
+            setAuthError(req.response.data.message)
+        },
+        onSettled: () => {
+            setIsCheckingAuth(false)
+        },
+    })
+
+    return {
+        useUpdateCredentials,
+        authToken,
+        isCheckingAuth,
+        authError
+    }
+}
