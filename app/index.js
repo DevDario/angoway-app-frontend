da115b6dd79eb194127ac19cdb02271c1923a5df
DevDario <dariosilva13222@gmail.com>
2025-04-12 22:08:49 +0000

    refactor: removes all the location request permission logic

--- app/index.js
+++ app/index.js
@@ -1,46 +1,12 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "expo-router";
-import {
-  requestForegroundPermissionsAsync,
-  getCurrentPositionAsync,
-  watchPositionAsync,
-  LocationAccuracy
-} from "expo-location";
 import QueryProvider from "../providers/QueryProvider";
 import { useAuth } from "../hooks/useAuth";
 import { ActivityIndicator, View } from "react-native";
 
 export default function Index() {
-  const [location, setLocation] = useState(null);
   const router = useRouter();
   const { authToken, isCheckingAuth } = useAuth()
-
-  async function getLocationPermission() {
-    const { granted } = await requestForegroundPermissionsAsync();
-
-    if (granted) {
-      const location = await getCurrentPositionAsync();
-      setLocation(location);
-    } else {
-      Alert.alert("Precisamos de permissão para acessar sua localização");
-    }
-  }
-
-  useEffect(() => {
-    getLocationPermission();
-  }, []);
-
-  useEffect(() => {
-    watchPositionAsync({
-      accuracy: LocationAccuracy.Highest,
-      timeInterval: 1000,
-      distanceInterval: 1,
-    }, (response) => {
-      setLocation(response)
-    })
-  }, [])
-
 
   //check if the user is authenticated
   useEffect(() => {
