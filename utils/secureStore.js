8b8d40f9e7430452418a46b3283b5688fcf59d20
Dário Silva <dariosilva13222@gmail.com>
2025-04-17 12:51:09 +0100

    refactor: uses async storage instead of MMKV due to incompatibility issues

--- utils/secureStore.js
+++ utils/secureStore.js
@@ -1,57 +1,56 @@
-import { MMKV } from "react-native-mmkv"
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const TOKEN_KEY = process.env.EXPO_PUBLIC_TOKEN_KEY || "access_token"
-const PROFILE_DATA_KEY = process.env.EXPO_PUBLIC_PROFILE_DATA_KEY
+const TOKEN_KEY = process.env.EXPO_PUBLIC_TOKEN_KEY || 'access_token'
+const PROFILE_DATA_KEY = process.env.EXPO_PUBLIC_PROFILE_DATA_KEY || 'profile_data'
 
-export const storage = new MMKV({
-    id: "secureStore",
-    //encryptionKey: process.env.EXPO_PUBLIC_MMKV_ENCRYPTION_KEY
-})
-
-export function saveToken(token) {
-    if (typeof window !== "undefined") {
-        localStorage.setItem(TOKEN_KEY, token);
-    } else {
-        storage.set(TOKEN_KEY, token);
+export async function saveToken(token) {
+    try {
+        await AsyncStorage.setItem(TOKEN_KEY, token)
+    } catch (error) {
+        console.error('Error saving token:', error)
     }
 }
 
-export function getToken() {
-    if (typeof window !== "undefined") {
-        return localStorage.getItem(TOKEN_KEY);
-    } else {
-        return storage.getString(TOKEN_KEY);
+export async function getToken() {
+    try {
+        return await AsyncStorage.getItem(TOKEN_KEY)
+    } catch (error) {
+        console.error('Error getting token:', error)
+        return null
     }
 }
 
-export function removeToken() {
-    if (typeof window !== "undefined") {
-        localStorage.removeItem(TOKEN_KEY);
-    } else {
-        storage.delete(TOKEN_KEY);
+export async function removeToken() {
+    try {
+        await AsyncStorage.removeItem(TOKEN_KEY)
+    } catch (error) {
+        console.error('Error removing token:', error)
     }
 }
 
-export function saveProfileData(data) {
-    if (typeof window !== "undefined") {
-        localStorage.setItem(PROFILE_DATA_KEY, JSON.stringify(data));
-    } else {
-        storage.set(TOKEN_KEY, JSON.stringify(data));
+export async function saveProfileData(data) {
+    try {
+        const jsonValue = JSON.stringify(data)
+        await AsyncStorage.setItem(PROFILE_DATA_KEY, jsonValue)
+    } catch (error) {
+        console.error('Error saving profile data:', error)
     }
 }
 
-export function getProfileData() {
-    if (typeof window !== "undefined") {
-        localStorage.getItem(PROFILE_DATA_KEY);
-    } else {
-        storage.get(TOKEN_KEY);
+export async function getProfileData() {
+    try {
+        const jsonValue = await AsyncStorage.getItem(PROFILE_DATA_KEY)
+        return jsonValue != null ? JSON.parse(jsonValue) : null
+    } catch (error) {
+        console.error('Error getting profile data:', error)
+        return null
     }
 }
 
-export function removeProfileData() {
-    if (typeof window !== "undefined") {
-        localStorage.removeItem(PROFILE_DATA_KEY);
-    } else {
-        storage.delete(PROFILE_DATA_KEY);
+export async function removeProfileData() {
+    try {
+        await AsyncStorage.removeItem(PROFILE_DATA_KEY)
+    } catch (error) {
+        console.error('Error removing profile data:', error)
     }
 }
