68295ab180dedf75650f85c8418442f75f0e9109
Dário Silva <dariosilva13222@gmail.com>
2025-04-24 15:01:12 +0100

    fix: attaches the user token on the socket object

--- app/api/socketInstance.js
+++ app/api/socketInstance.js
@@ -1,9 +1,14 @@
 import { io } from "socket.io-client";
+import { getToken } from "../../utils/secureStore";
 
 
 const BACKEND_URL = "http://172.20.10.4:3000"
 
 export const socket = io(BACKEND_URL, {
     autoConnect: false,
-    transports: ["websocket"]
+    transports: ["websocket"],
+    auth: async (cb) => {
+        const token = await getToken();
+        cb({ token });
+    },
 })
