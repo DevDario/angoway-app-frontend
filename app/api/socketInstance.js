c8c3b94edeec2d6966171be0df0d221c7dbdcae0
Dário Silva <dariosilva13222@gmail.com>
2025-04-24 13:42:00 +0100

    fix: changes backend url to local IP Address for Expo Go Connection

--- app/api/socketInstance.js
+++ app/api/socketInstance.js
@@ -1,9 +1,9 @@
 import { io } from "socket.io-client";
 
 
-const BACKEND_URL = process.env.EXPO_PUBLIC_BACKEND_URL || "https://angoway-app-backend.onrender.com"
+const BACKEND_URL = "http://172.20.10.4:3000"
 
 export const socket = io(BACKEND_URL, {
     autoConnect: false,
-    transports:["websocket"]
+    transports: ["websocket"]
 })
