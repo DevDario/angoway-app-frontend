fb4d1e1747942ddf445c8a7b5a052c769d073486
Dário Silva <dariosilva13222@gmail.com>
2025-04-30 18:40:44 +0100

    feat: creates new update user accessibility option use case fix: sets the token into the header of all uses cases

--- app/api/profile.js
+++ app/api/profile.js
@@ -1,10 +1,27 @@
+import { getToken } from "../../utils/secureStore";
 import { api } from "./axiosInstance";
 
 export const updateCredentialsUseCase = async ({ email, number }) => {
+    const token = await getToken()
     const response = await api.patch("/profile/", {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
         email,
         number
     })
+
+    return response.data
+}
+
+export const updateAccessibilityUseCase = async ({option}) => {
+    const token = await getToken()
+    const response = await api.patch("user/7", {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+        option
+    },)
 
     return response.data
 }
