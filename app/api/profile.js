3782ee79e5dc94c1fae4188af63838efb04b05aa
Dário Silva <dariosilva13222@gmail.com>
2025-05-03 21:21:59 +0100

    feat: creates new usecase to update user's password feat: creates an external config object to use in the request

--- app/api/profile.js
+++ app/api/profile.js
@@ -3,25 +3,46 @@ import { api } from "./axiosInstance";
 
 export const updateCredentialsUseCase = async ({ email, number }) => {
     const token = await getToken()
-    const response = await api.patch("/profile/", {
+    const config = {
         headers: {
+            Accept: '*/*',
             Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json'
         },
-        email,
-        number
-    })
+        email: email,
+        number: number
+    }
+    const response = await api.patch("/profile/", config)
 
     return response.data
 }
 
-export const updateAccessibilityUseCase = async ({option}) => {
+export const updateAccessibilityUseCase = async ({ option }) => {
     const token = await getToken()
-    const response = await api.patch("user/7", {
+    const config = {
         headers: {
+            Accept: '*/*',
             Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json'
         },
-        option
-    },)
+        option: option
+    }
+    const response = await api.patch("/user/", config)
+
+    return response.data
+}
+
+export const updatePasswordUseCase = async ({ password }) => {
+    const token = await getToken()
+    const config = {
+        headers: {
+            Accept: '*/*',
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        password: password
+    }
+    const response = await api.put("/user/profile/", config)
 
     return response.data
 }
