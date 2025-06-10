515324e8069a1c4ad1851d514b2b0cb87eac6f9d
Dário Silva <dariosilva13222@gmail.com>
2025-06-10 20:21:37 +0100

    chore: implements custom fonts and provide them globally

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,43 @@
 import { Stack } from "expo-router";
 import React from "react";
 import QueryProvider from "../providers/QueryProvider";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import { ActivityIndicator, Text, View } from "react-native";
+import { SplashScreen } from "expo-router";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Inter: require("../assets/fonts/InterRegular.ttf"),
+    "Inter-Bold": require("../assets/fonts/InterBold.ttf"),
+    "Inter-Light": require("../assets/fonts/InterLight.ttf"),
+    "Inter-Medium": require("../assets/fonts/InterMedium.ttf"),
+    "Inter-Regular": require("../assets/fonts/InterRegular.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        gap: 10
+      }}>
+        <ActivityIndicator style={{ justifyContent: "center", alignItems: "center", backgroundColor: "#FCFCB" }} size="large" color="#0C6BFF" />
+        <Text style={{
+          fontSize: 14,
+          fontWeight: "bold",
+          color: "#0C6BFF"
+        }}>Carregando</Text>
+      </View>
+    )
+  }
+
   return (
     <QueryProvider>
       <Stack
